7b606c59e6d1d057487c12716d9088b427ddd764
Your Name <you@example.com>
2017-12-19 14:50:37 -0800

    lll
    lll

--- src/java/simpledb/TupleDesc.java
+++ src/java/simpledb/TupleDesc.java
@@ -10,7 +10,7 @@ public class TupleDesc implements Serializable {
 
 	Type[] myTypeAr;
 	String[] myFieldAr;
-	//Rong is strong
+	//Rong is stronglll
 	
     /**
      * A help class to facilitate organizing the information of each field
