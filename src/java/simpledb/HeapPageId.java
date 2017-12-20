c73f407b999b2770920c8489abc1c7d5d011a075
鄂博维 <ebowei@eboweideMacBook-Pro.local>
2017-12-20 15:49:05 -0800

    Heappage
    Heappage

--- src/java/simpledb/HeapPageId.java
+++ src/java/simpledb/HeapPageId.java
@@ -10,14 +10,18 @@ public class HeapPageId implements PageId {
      * @param tableId The table that is being referenced
      * @param pgNo The page number in that table.
      */
+	private int tableId;
+	private int pgNo;
     public HeapPageId(int tableId, int pgNo) {
         // some code goes here
+    	this.tableId=tableId;
+    	this.pgNo=pgNo;
     }
 
     /** @return the table associated with this PageId */
     public int getTableId() {
         // some code goes here
-        return 0;
+        return tableId;
     }
 
     /**
@@ -26,7 +30,7 @@ public class HeapPageId implements PageId {
      */
     public int getPageNumber() {
         // some code goes here
-        return 0;
+        return pgNo;
     }
 
     /**
@@ -37,7 +41,8 @@ public class HeapPageId implements PageId {
      */
     public int hashCode() {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        String s=""+tableId+pgNo;
+        return Integer.parseInt(s);
     }
 
     /**
@@ -48,8 +53,10 @@ public class HeapPageId implements PageId {
      *   ids are the same)
      */
     public boolean equals(Object o) {
-        // some code goes here
-        return false;
+    	if(o == null) return false;
+    	if(!(o instanceof HeapPageId)) return false;
+    	HeapPageId temp=(HeapPageId) o;
+        return (tableId==temp.tableId && pgNo==temp.pgNo);
     }
 
     /**
