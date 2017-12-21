ec46f5efa457cf50a44ed75c59e8e427454d5956
鄂博维 <ebowei@eboweideMacBook-Pro.local>
2017-12-20 16:06:59 -0800

    recordid
    recordid

--- src/java/simpledb/RecordId.java
+++ src/java/simpledb/RecordId.java
@@ -19,8 +19,12 @@ public class RecordId implements Serializable {
      * @param tupleno
      *            the tuple number within the page.
      */
+    private PageId pid;
+    private int tupleno;
     public RecordId(PageId pid, int tupleno) {
         // some code goes here
+    	this.pid=pid;
+    	this.tupleno=tupleno;
     }
 
     /**
@@ -28,7 +32,7 @@ public class RecordId implements Serializable {
      */
     public int getTupleNumber() {
         // some code goes here
-        return 0;
+        return tupleno;
     }
 
     /**
@@ -36,7 +40,7 @@ public class RecordId implements Serializable {
      */
     public PageId getPageId() {
         // some code goes here
-        return null;
+        return pid;
     }
 
     /**
@@ -48,7 +52,10 @@ public class RecordId implements Serializable {
     @Override
     public boolean equals(Object o) {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
+    	if(o == null) return false;
+    	if(!(o instanceof RecordId)) return false;
+    	RecordId temp=(RecordId) o;
+        return (pid.equals(temp.pid) && tupleno==temp.tupleno);
     }
 
     /**
@@ -59,9 +66,9 @@ public class RecordId implements Serializable {
      */
     @Override
     public int hashCode() {
-        // some code goes here
-        throw new UnsupportedOperationException("implement this");
-
+        int c=pid.hashCode();
+        String s=""+c+tupleno;
+        return Integer.parseInt(s);
     }
 
 }
